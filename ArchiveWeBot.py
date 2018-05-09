f205265e842cbb500c5ad79987bd06dd664f3bff
TerminusBot <>
2018-05-09 22:18:40 +0800

    添加自己的分享

--- ArchiveWeBot.py
+++ ArchiveWeBot.py
@@ -7,15 +7,14 @@ import archiveis
 
 class MyWXBot(WXBot):
     def handle_msg_all(self, msg):
-        if (msg['msg_type_id'] == 4) and msg['content']['type'] == 7:
+        if (msg['msg_type_id'] == 1 or msg['msg_type_id'] == 4) and msg['content']['type'] == 7:
             self.send_msg_by_uid(archiveis.capture(msg['content']['data']['url']), msg['user']['id'])
             self.send_msg_by_uid(u'请复制上述链接在墙外打开', msg['user']['id'])
-
 
 def main():
     bot = MyWXBot()
     bot.DEBUG = True
-    bot.conf['tty'] = 'png' # 非 Linux 服务器环境请将 tty 改为 qr
+    bot.conf['qr'] = 'png' # 非 Linux 服务器环境请将 qr 改为 tty，目前发现有 bug
     bot.run()
 
 
